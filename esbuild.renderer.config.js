c1d9270a912edaf2bebcf55d811a928a5c35994d
murat <murat.ozsoyler@gmail.com>
2022-01-06 23:41:03 +0300

    Initial commit

---
+++ esbuild.renderer.config.js
@@ -0,0 +1,11 @@
+import * as path from 'path'
+
+/**
+ * @var {Partial<import('esbuild').BuildOptions>}
+ */
+export default {
+  platform: 'browser',
+  entryPoints: [path.resolve('src/renderer/index.jsx')],
+  bundle: true,
+  target: 'chrome96', // electron version target
+}
